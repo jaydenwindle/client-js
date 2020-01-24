ccdbf5160ce69f52b83171575556ea893da7bcd8
Jayden Windle <jaydenwindle@gmail.com>
2020-01-24 17:51:18 -0600

    moved rest transport to cross-fetch to add browser + RN support

--- src/rest/transport/request.ts
+++ src/rest/transport/request.ts
@@ -1,4 +1,4 @@
-import * as https from "https";
+import fetch from "cross-fetch";
 import * as querystring from "querystring";
 
 export interface IPolygonQuery {
@@ -11,42 +11,25 @@ export interface IPolygonQueryWithCredentials extends IPolygonQuery {
 
 export const auth = (apiKey, func) => (...args) => func(apiKey, ...args);
 
-export const get = (
+export const get = async (
   path: string,
   apiKey: string = "invalid",
   query?: IPolygonQuery
-): Promise<any> =>
-  new Promise((resolve, reject) => {
-    if (!apiKey) {
-      throw new Error("API KEY not configured...");
-    }
+): Promise<any> => {
+  if (!apiKey) {
+    throw new Error("API KEY not configured...");
+  }
 
-    const authenticatedQuery: IPolygonQueryWithCredentials = {
-      ...query,
-      apiKey
-    };
+  const authenticatedQuery: IPolygonQueryWithCredentials = {
+    ...query,
+    apiKey
+  };
 
-    const options = {
-      protocol: "https:",
-      method: "GET",
-      host: "api.polygon.io",
-      path: path + "?" + querystring.encode(authenticatedQuery)
-    };
+  const url = `https://api.polygon.io${path}?${querystring.encode(
+    authenticatedQuery
+  )}`;
 
-    const request = https.request(options, (response: any) => {
-      let data = "";
-      response.on("data", function(chunk: string) {
-        data += chunk;
-      });
+  const response = await fetch(url);
 
-      response.on("end", function() {
-        resolve(JSON.parse(data));
-      });
-    });
-
-    request.on("error", (error: any) => {
-      reject(error);
-    });
-
-    request.end();
-  });
+  return response.json();
+};
