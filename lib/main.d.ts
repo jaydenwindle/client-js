d17f9a511b5963fd16b88073ced9d18db41e8537
Jayden Windle <jaydenwindle@gmail.com>
2020-01-25 09:19:17 -0600

    updated type export, updated lib

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,5 +1,10 @@
 export * from "./rest";
 export * from "./websockets";
-import { IPolygonClient } from "./main.d";
+import { IRestClient } from "./rest/index.d";
+import { IWebsocketClient } from "./websockets/index.d";
+export interface IPolygonClient {
+  rest: IRestClient;
+  websockets: IWebsocketClient;
+}
 export declare const polygonClient: (apiKey: string) => IPolygonClient;
 export default polygonClient;
