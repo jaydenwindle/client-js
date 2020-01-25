f57bfdf845cff2ddb2d575c82bf079aeee5c378e
Jayden Windle <jaydenwindle@gmail.com>
2020-01-25 10:15:15 -0600

    apply type fixes

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -2,6 +2,16 @@ export * from "./rest";
 export * from "./websockets";
 import { IRestClient } from "./rest/index.d";
 import { IWebsocketClient } from "./websockets/index.d";
+export * from "./rest/index.d";
+export * from "./rest/crypto/index.d";
+export * from "./rest/forex/index.d";
+export * from "./rest/stocks/index.d";
+export * from "./rest/reference/index.d";
+export * from "./websockets/index.d";
+export {
+  IAggResponseFormatted,
+  IAggregateQuery
+} from "./rest/stocks/aggregates";
 export interface IPolygonClient {
   rest: IRestClient;
   websockets: IWebsocketClient;
